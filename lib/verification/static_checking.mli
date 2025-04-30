5ccf5da4b0790dc5155854ffce46ce8c3bcbe365
Diogo <d.ye@campus.fct.unl.pt>
2025-04-30 11:13:31 +0100

    [Fix] Static information flow control (stable version)

--- lib/verification/static_checking.mli
+++ lib/verification/static_checking.mli
@@ -1,6 +1,8 @@
+module TreeMap : module type of Map.Make (String)
 open Frontend.Syntax
 open Utils
 
 exception Typecheck_IFC_error of string
 
-val check_static_information_security : program -> ( program,(loc * string) list )result
+
+val check_static_information_security : program -> ( Sat.cnf_formula TreeMap.t,(loc * string) list )result
