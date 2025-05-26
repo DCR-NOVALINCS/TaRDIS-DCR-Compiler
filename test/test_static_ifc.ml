8ff215511c19cbb4fb63f54838c12ca3ac94d9f2
Diogo <d.ye@campus.fct.unl.pt>
2025-05-26 09:33:08 +0100

    [Tests]: Add tests

--- test/test_static_ifc.ml
+++ test/test_static_ifc.ml
@@ -1,22 +1,15 @@
-(* open OUnit2
+open OUnit2
 open Common
 (** Helper functions *)
 open Verification
 module TreeMap = Map.Make (String)
 
 let static_ifc_checking_from_prog_state prog_state =
-  Verification.Preprocessing.preprocess_program prog_state
-  >>= fun preprocessing_res ->
-  Verification.Typing.check_program preprocessing_res >>= fun typecheck_res ->
-  Static_checking.check_static_information_security prog
+  let open Utils.Results in
+  Verification.Preprocessing.preprocess_program prog_state >>= fun preprocessing_res ->
+  Verification.Typing.check_program preprocessing_res >>= fun _ ->
+  Static_checking.check_static_information_security prog_state
 
-(* let error_msg result =
-  match result with
-  | Ok _ -> ""
-  | Error result_log ->
-    List.fold_left (fun acc (loc, msg) -> acc ^ ": " ^ msg ^ "\n") "" result_log *)
-
-(** Tests *)
 
 let test_case prog_state exp err_msg =
   let ty = static_ifc_checking_from_prog_state prog_state in
@@ -28,26 +21,42 @@ let test_case prog_state exp err_msg =
 
 (** Test Suite *)
 let test_suite =
-  "test suite for static information flow control"
+  "test suite for static information flow control no params"
   >::: [ 
-    ( "Simple testing" >:: fun test_ctxt ->
+    ( "Simple test for relation and event data" >:: fun test_ctxt ->
            let state = build_state "resources/static_checking/0.tardisdcr" test_ctxt in
            test_case state (Ok  TreeMap.empty ) "Expected ok" )
        ;
-        (* ( "Testing IFC type expr in input events" >:: fun test_ctxt ->
+    ( "Testing IFC type expr in input events" >:: fun test_ctxt ->
            let state = build_state "resources/static_checking/0.1.tardisdcr" test_ctxt in
            test_case state (Error []) "Expected Fail" )
         ; 
-        ("Testing IFC " >:: fun test_ctxt ->
+    ("Testing IFC type expr in input events + relation" >:: fun test_ctxt ->
             let state = build_state "resources/static_checking/0.1.1.tardisdcr" test_ctxt in
                 test_case state ( Ok TreeMap.empty ) "Expected Ok in Test 0.1.1") 
-          ; *)
-          (* ( "Testing IFC " >::
+     ;
+    ( "Testing IFC 0.1.1 but with leak in relations" >::
             (fun test_ctxt ->
-              let state = build_state "resources/static_checking/1.1.tardisdcr" test_ctxt in
-              test_case state ( Ok TreeMap.empty ) "Expected Ok in Test 0.1.2") )
-              
-              ; *)
-       ]
+              let state = build_state "resources/static_checking/0.1.2.tardisdcr" test_ctxt in
+              test_case state ( Error []) "Expected Error in relation") )
+      ;
+    ( "Testing IFC with multiple security labels" >::
+            (fun test_ctxt ->
+              let state = build_state "resources/static_checking/0.1.3.tardisdcr" test_ctxt in
+              test_case state ( Ok TreeMap.empty ) "Expected Ok in Test 0.1.3") )
+      ;
+      ( "Testing simple IFC with spawns" >::
+            (fun test_ctxt ->
+              let state = build_state "resources/static_checking/0.2.tardisdcr" test_ctxt in
+              test_case state ( Ok TreeMap.empty ) "Expected Ok in Test 0.2") )
+      ;
+      ( "Testing simple IFC with spawns" >::
+      (fun test_ctxt ->
+        let state = build_state "resources/static_checking/0.2.tardisdcr" test_ctxt in
+        test_case state ( Ok TreeMap.empty ) "Expected Ok in Test 0.2") )
+;
 
-let _ = run_test_tt_main test_suite *)
+   
+  ]
+
+let _ = run_test_tt_main test_suite
